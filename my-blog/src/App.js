aca738ebb395aadfa38830fb43a449c66ba41644
jonathan-shamis-nagarro <jonathans607@nycstudents.net>
2024-04-23 14:12:40 -0400

    Created each page component and added routing to each of them

---
+++ my-blog/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Awesome Blog</h1>
+        <div id='page-body'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/articles' element={<ArticlesListPage />} />
+            <Route path='/articles/:articleId' element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
