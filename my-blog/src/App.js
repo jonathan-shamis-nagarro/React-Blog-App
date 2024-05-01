47b6d1532e9eeb91547af7d97bec52e41603f297
jonathan-shamis-nagarro <jonathans607@nycstudents.net>
2024-05-01 14:10:37 -0400

    Created UI for Login Page.

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -8,6 +8,8 @@ import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import CreateAccountPage from './pages/CreateAccountPage';
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
             <Route path='/about' element={<AboutPage />} />
             <Route path='/articles' element={<ArticlesListPage />} />
             <Route path='/articles/:articleId' element={<ArticlePage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/create-account' element={<CreateAccountPage />} />
             <Route path='*' element={<NotFoundPage />} />
           </Routes>
         </div>
