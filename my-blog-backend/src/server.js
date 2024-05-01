422dec4082a12f9759b9a4c2751f350bb028d334
jonathan-shamis-nagarro <jonathans607@nycstudents.net>
2024-05-01 14:59:04 -0400

    Tweaked backend endpoints

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import { db, connectToDb } from './db.js';
 
 const credentials = JSON.parse(
-    fs.readFileSync('../credentials.json')
+    fs.readFileSync('./credentials.json')
 );
 admin.initializeApp({
     credential: admin.credential.cert(credentials),
@@ -13,28 +13,62 @@ admin.initializeApp({
 const app = express();
 app.use(express.json());
 
+app.use(async (req, res, next) => {
+    const { authtoken } = req.headers;
+
+    if (authtoken){
+        try {
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        } catch (e) {
+            res.sendStatus(400);
+        }
+    }
+
+    next();
+});
+
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid);
         res.json(article);
     } else {
         res.sendStatus(404);
     }
 });
 
+app.use((req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+});
+
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
+    const uid = req.user;
     
-    await db.collection('articles').updateOne({ name }, {
-        $inc: { upvotes: 1},
-    });
-    const article = await db.collection('react-blog-db').findOne({ name });
+    const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        res.json(article);
+        const upvoteIds = article.upvoteIds || [];
+        const canUpvote = uid && !upvoteIds.include(uid);
+
+        if (canUpvote) {
+            await db.collection('articles').updateOne({ name }, {
+                $inc: { upvotes: 1 },
+                $push: { upvoteIds: uid }
+            });
+        }
+
+        const updatedArticle = await db.collection('react-blog-db').findOne({ name });
+        res.send(updatedArticle);
     } else {
         res.send('That article doesn\'t exist');
     }
@@ -42,10 +76,11 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 
 app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
-    const { postedBy, text } = req.body;
+    const { text } = req.body;
+    const { email } = req.user;
 
     await db.collection('articles').updateOne({ name }, {
-        $push: { comments: { postedBy, text } },
+        $push: { comments: { postedBy: email, text } },
     });
     const article = await db.collection('articles').findOne({ name });
 
